5b86306f723c8cd753e3b6b8b112a91b005529c1
Yeoh, Ewe Seong <eweseong.y@gmail.com>
2021-06-23 11:49:13 +0800

    build(npm): copy library metadata to dist

---
+++ scripts/postBuild.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const pkg = require('../package.json');
+
+const root = path.resolve(__dirname, '..');
+const dist = path.resolve(__dirname, '../dist');
+
+const neededParams = [
+  'name',
+  'version',
+  'description',
+  'main',
+  'typings',
+  'repository',
+  'keywords',
+  'author',
+  'license',
+  'bugs',
+  'dependencies',
+  'peerDependencies',
+  'module',
+  'exports',
+];
+
+Object.keys(pkg).forEach((param) => {
+  if (!neededParams.includes(param)) {
+    delete pkg[param];
+  }
+});
+
+fs.writeFileSync(
+  path.resolve(dist, 'package.json'),
+  JSON.stringify(pkg, null, 4)
+);
+console.info('✅ Created package.json in dist folder');
+
+fs.copyFileSync(
+  path.resolve(root, 'README.md'),
+  path.resolve(dist, 'README.md')
+);
+console.info('✅ Copied over README.md to dist');
+
+fs.copyFileSync(path.resolve(root, 'LICENSE'), path.resolve(dist, 'LICENSE'));
+console.info('✅ Copied over LICENSE to dist');
