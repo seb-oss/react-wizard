ac5a59242dceaf88a1a241fe0971a444353f0562
Yeoh, Ewe Seong <eweseong.y@gmail.com>
2021-06-24 18:00:15 +0800

    docs(storybook): add wizard header stories

--- src/stories/WizardHeader.stories.tsx
+++ src/stories/WizardHeader.stories.tsx
@@ -2,15 +2,37 @@ import { Meta, Story } from '@storybook/react';
 import React from 'react';
 import WizardHeader, { WizardHeaderProps } from '../WizardHeader';
 
+const actionsOptions = {
+  'Single action': [<button className="btn">x</button>],
+  'Multiple actions': [
+    <button className="btn">Save</button>,
+    <button className="btn">x</button>,
+  ],
+};
+
 export default {
   title: 'components/WizardHeader',
   component: WizardHeader,
-  argTypes: {},
+  argTypes: {
+    actions: {
+      options: Object.keys(actionsOptions),
+      mapping: actionsOptions,
+    },
+  },
 } as Meta;
 
 const Template: Story<WizardHeaderProps> = (args) => <WizardHeader {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  heading: 'Wizard Header',
+  heading: 'Wizard header',
+};
+
+export const WithActions = Template.bind({});
+WithActions.args = {
+  heading: 'Wizard header with actions',
+  actions: [
+    <button className="btn">Save</button>,
+    <button className="btn">x</button>,
+  ],
 };
