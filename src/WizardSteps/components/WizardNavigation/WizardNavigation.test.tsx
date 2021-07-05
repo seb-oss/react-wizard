5330cc9e0c5f2feb37137149c7e9c9d2892f7a20
Yeoh, Ewe Seong <eweseong.y@gmail.com>
2021-07-05 20:54:01 +0800

    feat(wizard-navigation): add wizard navigation component

---
+++ src/WizardSteps/components/WizardNavigation/WizardNavigation.test.tsx
@@ -0,0 +1,133 @@
+import {
+  fireEvent,
+  render,
+  RenderResult,
+  screen,
+} from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import {
+  NavigationInterface,
+  useNavigationContext,
+} from '../../../contexts/navigationContext';
+import WizardNavigation, { WizardNavigationProps } from './WizardNavigation';
+
+jest.mock('../../../contexts/navigationContext', () => ({
+  useNavigationContext: jest.fn(),
+}));
+
+const mockedUseNavigationContext = useNavigationContext as jest.Mock<NavigationInterface>;
+
+describe('Component: WizardNavigation', () => {
+  const defaultNavigationContext: NavigationInterface = {
+    activeStep: 0,
+    activeState: undefined,
+    isValidStep: jest.fn(),
+    nextStep: jest.fn(),
+    previousStep: jest.fn(),
+    setActiveState: jest.fn(),
+    setActiveStep: jest.fn(),
+  };
+  const wizardNavigationProps: WizardNavigationProps = {
+    label: 'Step 1',
+    path: '/step1',
+    step: 0,
+  };
+
+  function renderWithRouter(
+    props?: Partial<WizardNavigationProps>
+  ): RenderResult {
+    return render(
+      <MemoryRouter initialEntries={['/']}>
+        <WizardNavigation {...{ ...wizardNavigationProps, ...props }} />
+      </MemoryRouter>
+    );
+  }
+
+  beforeEach(() => {
+    mockedUseNavigationContext.mockImplementation(
+      () => defaultNavigationContext
+    );
+  });
+
+  it('Should render correctly', () => {
+    renderWithRouter();
+    expect(screen.getByText(wizardNavigationProps.label)).toBeInTheDocument();
+  });
+
+  it('Should render active state', () => {
+    const { container } = renderWithRouter();
+    expect(
+      container.querySelector('.wizard-navigation--active')
+    ).toBeInTheDocument();
+  });
+
+  it('Should render completed state', () => {
+    mockedUseNavigationContext.mockImplementation(() => ({
+      ...defaultNavigationContext,
+      activeStep: 1,
+    }));
+    const { container } = renderWithRouter();
+    expect(
+      container.querySelector('.wizard-navigation--passed')
+    ).toBeInTheDocument();
+  });
+
+  it('Should render invalid state', () => {
+    mockedUseNavigationContext.mockImplementation(() => ({
+      ...defaultNavigationContext,
+      isValidStep: jest.fn().mockReturnValueOnce(false),
+    }));
+    const { container } = renderWithRouter();
+    expect(
+      container.querySelector('.wizard-navigation--disabled')
+    ).toBeInTheDocument();
+  });
+
+  it('Should render danger state', () => {
+    const { container } = renderWithRouter({ state: 'danger' });
+    expect(
+      container.querySelector('.wizard-navigation--disabled')
+    ).toBeInTheDocument();
+  });
+
+  it('Should render info state', () => {
+    const { container } = renderWithRouter({ state: 'info' });
+    expect(
+      container.querySelector('.wizard-navigation--info')
+    ).toBeInTheDocument();
+  });
+
+  it('Should render warning state', () => {
+    const { container } = renderWithRouter({ state: 'warning' });
+    expect(
+      container.querySelector('.wizard-navigation--warning')
+    ).toBeInTheDocument();
+  });
+
+  it('Should render success state', () => {
+    const { container } = renderWithRouter({ state: 'success' });
+    expect(
+      container.querySelector('.wizard-navigation--success')
+    ).toBeInTheDocument();
+  });
+
+  it('Should render navigation context active state', () => {
+    mockedUseNavigationContext.mockImplementation(() => ({
+      ...defaultNavigationContext,
+      activeState: 'danger',
+    }));
+    const { container } = renderWithRouter();
+    expect(
+      container.querySelector('.wizard-navigation--danger')
+    ).toBeInTheDocument();
+  });
+
+  it('Should trigger navigation click handler when provided', () => {
+    const mockedOnClick = jest.fn();
+    renderWithRouter({ onClick: mockedOnClick });
+    expect(mockedOnClick).not.toHaveBeenCalled();
+    fireEvent.click(screen.getByText(wizardNavigationProps.label));
+    expect(mockedOnClick).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/WizardSteps/components/WizardNavigation/WizardNavigation.tsx
@@ -0,0 +1,61 @@
+import classnames from 'classnames';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useNavigationContext } from '../../../contexts/navigationContext';
+import { WizardStepState } from '../WizardStep';
+import './WizardNavigation.scss';
+
+export type WizardNavigationData = {
+  /**
+   * The path of the navigation, a string representation of the location,
+   * created by concatenating the location’s pathname, search, and hash
+   * properties.
+   * */
+  path: string;
+  /** The label of the step in the navigation links. */
+  label: string;
+  /**
+   * Set state for the navigation, it is used to highlight and clarify
+   * wizard progress - completed, warnings, errors etc. By default
+   * navigation will be marked as finished or completed when
+   * you've passed them.
+   * */
+  state?: WizardStepState;
+};
+
+export type WizardNavigationProps = WizardNavigationData & {
+  /** The step number of the current step */
+  step: number;
+  /** Event triggered when navigation is click. */
+  onClick?: () => void;
+};
+
+const WizardNavigation: React.FC<WizardNavigationProps> = (props) => {
+  const { activeState, activeStep, isValidStep } = useNavigationContext();
+  const { path, label, step, onClick } = props;
+  const isActive: boolean = activeStep === step;
+  const isCompleted: boolean = activeStep > step;
+  const isValid: boolean = isValidStep(step);
+  const state: WizardStepState = isActive
+    ? activeState || props.state
+    : undefined;
+  return (
+    <li
+      className={classnames('list-group-item', 'wizard-navigation', {
+        'wizard-navigation--active': isActive,
+        'wizard-navigation--passed': isCompleted,
+        'wizard-navigation--disabled': !isValid,
+        'wizard-navigation--danger': state === 'danger',
+        'wizard-navigation--info': state === 'info',
+        'wizard-navigation--warning': state === 'warning',
+        'wizard-navigation--success': state === 'success',
+      })}
+    >
+      <NavLink to={path} onClick={onClick}>
+        {label}
+      </NavLink>
+    </li>
+  );
+};
+
+export default React.memo(WizardNavigation);
