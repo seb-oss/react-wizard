732023bdbb0ba95ae4b12e26c21858eeec93cc73
Yeoh, Ewe Seong <eweseong.y@gmail.com>
2021-07-14 16:03:08 +0800

    feat(navigation-context): manage active controls and provide new helper to assert route guard status

--- src/contexts/navigationContext.tsx
+++ src/contexts/navigationContext.tsx
@@ -1,14 +1,58 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { WizardControl } from '../WizardSteps/components/WizardControls';
 import { WizardStepState } from '../WizardSteps/components/WizardStep';
 
 export interface NavigationInterface {
+  /**
+   * Controls/actions for the current active step.
+   * */
+  activeControls: Array<WizardControl> | undefined;
+  /**
+   * State for the active step.
+   */
   activeState: WizardStepState;
+  /**
+   * Step number for the active step; based on zero-based numbering system.
+   */
   activeStep: number;
-  isValidStep: (step: number) => boolean;
+  /**
+   * Verify if the active step's next step handler is valid, it returns a
+   * promise based truthy value when it is valid, false otherwise.
+   */
+  isValidStep: () => Promise<boolean>;
+  /**
+   * Verify if the step provided is the immediate next step or any of the
+   * previous step. A step is considered navigable if it satisfy any of the
+   * conditions mentioned above.
+   */
+  isNavigableStep: (step: number) => boolean;
+  /**
+   * A navigation helper to navigation to next step. If a path is provided,
+   * it will navigate to the path provided, otherwise it will load the next
+   * step in the route list.
+   */
   nextStep: (path?: string) => void;
+  /**
+   * A navigation helper to navigation to previous step. If a path is provided
+   * it will navigate to the path provided, otherwise it will load the previous
+   * step in the route list
+   */
   previousStep: (path?: string) => void;
+  /**
+   * Configure the controls/actions for the current active step.
+   */
+  setActiveControls: React.Dispatch<
+    React.SetStateAction<Array<WizardControl> | undefined>
+  >;
+  /**
+   * Configure the state for the active step.
+   */
   setActiveState: React.Dispatch<React.SetStateAction<WizardStepState>>;
+  /**
+   * Configure the step number for the active step;
+   * based on zero-based numbering system.
+   */
   setActiveStep: React.Dispatch<React.SetStateAction<number>>;
 }
 
@@ -26,13 +70,28 @@ const NavigationProvider: React.FC<NavigationProviderProps> = ({
   routes,
 }) => {
   const history = useHistory();
+  const [activeControls, setActiveControls] = React.useState<
+    Array<WizardControl>
+  >();
   const [activeState, setActiveState] = React.useState<WizardStepState>();
   const [activeStep, setActiveStep] = React.useState<number>(0);
 
-  const isValidStep = React.useCallback(
+  const isNavigableStep = React.useCallback(
     (step: number) => step <= activeStep + 1,
     [activeStep]
   );
+
+  const isValidStep = React.useCallback(async () => {
+    const nextStepHandler = activeControls?.find(
+      (control: WizardControl) => control.type === 'next'
+    )?.onClick;
+
+    if (typeof nextStepHandler === 'function') {
+      return await nextStepHandler();
+    }
+
+    return Promise.resolve(true);
+  }, [activeControls]);
 
   const nextStep = React.useCallback(
     (path?: string) => {
@@ -61,11 +120,14 @@ const NavigationProvider: React.FC<NavigationProviderProps> = ({
   return (
     <NavigationContext.Provider
       value={{
+        activeControls,
         activeState,
         activeStep,
         isValidStep,
+        isNavigableStep,
         nextStep,
         previousStep,
+        setActiveControls,
         setActiveState,
         setActiveStep,
       }}
